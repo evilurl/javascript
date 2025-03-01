c3dae4adf3d75692e65f2d14decc991281d56632
Muhammad Hassan Raza <141415306+evilurl@users.noreply.github.com>
2025-03-01 11:20:03 +0500

    String concatenation in JS.

---
+++ 02. String Concatenation - Counter.js
@@ -0,0 +1,10 @@
+var tweet = prompt('Write Your Tweet here!');
+var tweetSlicing = (tweet.slice(0,140));
+alert(tweetSlicing);
+// alert (
+//   'The length of your Tweet is ' +
+//     tweetLength +
+//     " & you've " +
+//     (140 - tweetLength) +
+//     ' characters remaining.'
+// );
